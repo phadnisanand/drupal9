f9c92d161b64ac8736508c9d051b0d642b04f104
Anand Phadnis <anandphadnis@Anands-MacBook-Pro.local>
2023-02-07 15:15:44 +0530

    custom block

---
+++ customblock_3/js/custom.js
@@ -0,0 +1,23 @@
+(function ($, Drupal) {
+  Drupal.behaviors.mycustomblock = {
+    attach: function (context) {
+      $("#edit-title").change(function (event) {
+        var jsonObjects = {title: $(this).val()};
+        $.ajax({
+          url: Drupal.url('ajaxresponse'),
+          type: 'POST',
+          dataType: 'json',
+          data: JSON.stringify(jsonObjects),
+          success: function (response) {
+            //console.log(response); 
+            $("#temp").text(response.temp);
+            $("#feels_like").text(response.feels_like);
+            $("#temp_min").text(response.temp_min);
+            $("#temp_max").text(response.temp_max);
+            $("#location").text(response.location);
+          }
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
