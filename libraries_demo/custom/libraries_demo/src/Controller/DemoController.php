b6fa667a5f728bbba0a9e6897dc622337c8cefa6
Anand Phadnis <anandphadnis@Anands-MacBook-Pro.local>
2023-03-07 01:06:10 +0530

    drupal 9

--- libraries_demo/custom/libraries_demo/src/Controller/DemoController.php
+++ libraries_demo/custom/libraries_demo/src/Controller/DemoController.php
@@ -3,6 +3,7 @@
 namespace Drupal\libraries_demo\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\libraries_demo\Demo as Demo;
+use Coduo\ToString\StringConverter;
 
 /**
  * Defines DemoController class.
@@ -33,4 +34,15 @@ class DemoController extends ControllerBase {
 		echo $obj->getName();
 		exit;
 	}
+
+	public function getlib() {
+		// https://packagist.org/packages/coduo/php-to-string
+		$datetime = new StringConverter(new \DateTime());
+		echo $datetime; // "\DateTime"
+
+		$array = new StringConverter(['foo', 'bar', 'baz']);
+		echo $array; // "Array(3)"
+		exit;
+	}
+	
 }
